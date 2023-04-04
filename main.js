3891dcb81958aa68d873a637a8a136a6e7970cc2
Albert Chae <albertchae@users.noreply.github.com>
2023-04-04 03:13:04 -0700

    Remove unnecessary insertLineBreak handling

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
 const mathField = document.querySelector('#math-field');
 
-mathField.addEventListener('input', (event) => {
-  // input event with insertLineBreak handles enter keypress from
-  // both physical and virtual keyboard
-  if (event.data == "insertLineBreak") {
-    console.log("got enter")
-  }
-});
-
 mathVirtualKeyboard.show()
 mathField.focus()
